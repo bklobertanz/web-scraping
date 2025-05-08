8219151f2f6484e65d5cd97d8c28830f15ac2707
bk <klobertanz@gmail.com>
2025-05-08 18:22:18 -0400

    refactor: create function

--- download_csv.py
+++ download_csv.py
@@ -9,19 +9,22 @@ from time import sleep
 # Setup Firefox driver
 service = Service(GeckoDriverManager().install())
 driver = webdriver.Firefox(service=service)
-
-
-url = "https://sinca.mma.gob.cl/cgi-bin/APUB-MMA/apub.htmlindico2.cgi?page=pageRight&header=Arica&gsize=1495x708&period=specified&from=00010100&to=25050523&macro=./RXV/F01/Cal/PM25//PM25.diario.diario.ic&limgfrom=&limgto=&limdfrom=&limdto=&rsrc=&stnkey="
+# URL generada en el paso anterior (scraping-mapeo.py)
+url = "https://sinca.mma.gob.cl/cgi-bin/APUB-MMA/apub.htmlindico2.cgi?page=pageRight&header=Arica&gsize=1495x708&period=specified&from=000101&to=250506&macro=./RXV/F01/Cal/PM25//PM25.diario.anual.ic&limgfrom=&limgto=&limdfrom=&limdto=&rsrc=&stnkey="
 driver.get(url)
 
 # Ejecutando funcion Open para descargar archivo csv
-try:
+
+
+def download_csv(driver, url):
     sleep(5)  # Esperar 5 segundos para que la página cargue completamente
-    csvParam = "xcl"
-    args = [csvParam]
     csvFileSel = "body > table > tbody > tr > td > table:nth-child(3) > tbody > tr:nth-child(1) > td > label > span.icon-file-excel > a"
     driver.find_element(By.CSS_SELECTOR, csvFileSel).click()
     sleep(10)  # Esperar 10 segundos para que el archivo se descargue
+
+
+try:
+    download_csv(driver, url)
 
 except Exception as e:
     print(f"Error: {e}")
